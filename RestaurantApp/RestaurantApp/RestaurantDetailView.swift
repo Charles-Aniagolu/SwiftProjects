30ce5ada17d84d66e6db9bd403fdf8ffe562a8cf
Charles-Aniagolu <charles.nebo@hotmail.com>
2025-02-20 16:01:19 +0200

    Removed unnecessary codes in RestaurantDetailView file

--- RestaurantApp/RestaurantApp/RestaurantDetailView.swift
+++ RestaurantApp/RestaurantApp/RestaurantDetailView.swift
@@ -6,49 +6,6 @@
 //
 
 import SwiftUI
-/*
-struct RestaurantDetailView: View {
-    var restaurant: Restaurant
-    
-    var body: some View {
-        ScrollView {
-           
-             VStack(alignment: .leading) {
-             // Restaurant Image (Affects all restaurants)
-             Image(restaurant.imageName) // Uses dynamic restaurant images
-             .resizable()
-             .scaledToFill() // Maintains aspect ratio, prevents stretching
-             .frame(width: UIScreen.main.bounds.width - 20, height: 200) // Adjust size
-             .clipShape(RoundedRectangle(cornerRadius: 8)) // Keeps rounded corners
-             .shadow(radius: 2) // Adds shadow effect
-             .padding(.horizontal, 10) // Keeps slight margin on the sides
-             .offset(y: -2) // Moves image closer to title
-             
-             
-             // Restaurant Name
-             Text(restaurant.name)
-             .font(.largeTitle)
-             .fontWeight(.bold)
-             .padding(.horizontal)
-             
-             // Restaurant Description
-             Text(restaurant.description)
-             .font(.body)
-             .padding()
-             
-             Spacer()
-               }
-             }
-             .navigationBarTitle(restaurant.name, displayMode: .inline)
-        }
-    }
-     */
-            
-            
-            
-
-            
-import SwiftUI
 
 struct RestaurantDetailView: View {
     var restaurant: Restaurant
